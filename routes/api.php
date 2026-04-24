6893b2b8d320c9200e9787adc0d2ff99f72bd4a7
ahfifauka <ahfifauka.id@gmail.com>
2026-04-24 07:12:26 +0700

    first init

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::prefix('products')->controller(ProductController::class)->group(function () {
-    Route::get('/', 'getProducts');
-    Route::get('/{productId}', 'getProduct');
-});
-
-Route::prefix('categories')->controller(CategoryController::class)->group(function () {
-    Route::get('/', 'getCategories');
-});
-
-// csrf
